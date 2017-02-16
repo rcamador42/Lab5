4473adb21807524233ddfb3848d385d27919a197
Rayvin Carl Amador <rcamador42@students.tntech.edu>
2017-02-16 08:35:42 -0600

    First few files

---
+++ SortedListLinkedDriver.cpp
@@ -0,0 +1,46 @@
+#include "SortedListLinked.h"
+#include "CD.h"
+using CSC2110::CD;
+#include "ListArray.h"
+using CSC2110::ListArray;
+#include "ListArrayIterator.h"
+using CSC2110::ListArrayIterator;
+#include <iostream>
+using namespace std;
+
+void addCDs(ListArray<CD>* list, SortedListLinked<CD>* lb)
+{
+   ListArrayIterator<CD>* iter = list->iterator();
+
+   //DO THIS
+   //iterate over and add the cds to the list box (use lb->addItem)
+
+
+
+
+   delete iter;
+}
+
+void deleteCDs(ListArray<CD>* list)
+{
+   ListArrayIterator<CD>* iter = list->iterator();
+
+   //DO THIS  
+   //iterate over and delete the cds
+
+
+
+
+   delete iter;
+}
+
+int main(int argc, char* argv[])
+{
+   ListArray<CD>* cds = CD::readCDs("cds.txt");
+   //DO THIS
+   //create and test the sorted linked list 
+
+
+
+   return 0;
+}
